497f9b8993c21b313e8871a907f4c58a875df47e
mwash-fred <mwangiwilly395@gmail.com>
2023-02-08 05:34:11 +0300

    First Commit
    Took 2 minutes

--- src/main/java/co/wmc/datacleaner/Resources/ApiEndpoints.java
+++ src/main/java/co/wmc/datacleaner/Resources/ApiEndpoints.java
@@ -58,10 +58,10 @@ public class ApiEndpoints {
     @PostMapping("loan-account-sn")
     public ResponseEntity<?> updateLoanWithAcountSn(@RequestParam("file") MultipartFile file) throws SQLException, IOException {
         Map<String, Object> data = excelReader.disbursemetDateHandler(file);
-        for (String key : data.keySet()) {
-            //Update Accounts
-            customEntity.updateLoansWithAccountSn(Integer.parseInt(key), Integer.parseInt((String) data.get(key)));
-        }
+//        for (String key : data.keySet()) {
+//            //Update Accounts
+//            customEntity.updateLoansWithAccountSn(Integer.parseInt(key), Integer.parseInt((String) data.get(key)));
+//        }
 
         return ResponseEntity.ok().body(
                 CustomResponse.builder()
